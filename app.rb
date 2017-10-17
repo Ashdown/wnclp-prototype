6e41321932b6c712fd9fd194624b6af02b6db28c
Rory Devane <rory.devane@gmail.com>
2017-10-18 00:41:43 +0100

    added page content

--- app.rb
+++ app.rb
@@ -5,22 +5,26 @@ get '/' do
   erb :home
 end
 
-get '/who-we-are' do
-  erb :who_we_are
-end
-
-get '/ward-spotlight' do
-  erb :ward_spotlight
-end
-
-get '/elections' do
-  erb :elections
-end
-
 get '/campaigns' do
   erb :campaigns
 end
 
-get '/links' do
-  erb :links
+get '/volunteer' do
+  erb :volunteer
+end
+
+get '/elected-representatives' do
+  erb :elected_representatives
+end
+
+get '/officers-and-contacting-us' do
+  erb :officers_and_contacting_us
+end
+
+get '/member-meetings' do
+  erb :member_meetings
+end
+
+get '/donate' do
+  erb :donate
 end
